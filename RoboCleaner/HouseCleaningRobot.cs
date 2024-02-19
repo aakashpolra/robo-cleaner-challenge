783e2e50366eb2e5ee830f3652f89f3e7570efaf
Aakash <1733759+aakashpolra@users.noreply.github.com>
2024-02-19 23:38:10 +1300

    Crude implementation with given test cases (failing).

--- RoboCleaner/HouseCleaningRobot.cs
+++ RoboCleaner/HouseCleaningRobot.cs
@@ -1,7 +1,9 @@
 ﻿namespace RoboCleaner;
 
-public record Position(int Row, int Column);
 public enum Direction { Right = 0, Up = 1, Left = 2, Down = 3 }
+public record Position(int Row, int Column, Direction Direction);
+
+public record MovementResult (int NumCellsMoved, Position NewPosition);
 
 public class Constants
 {
@@ -29,8 +31,8 @@ public class Cell()
 public class HouseCleaningRobot
 {
     private readonly Cell[][] _grid;
+    private const Direction StartingDirection = Direction.Right;
     private Position _startingPosition;
-    private const Direction startingDirection = Direction.Right;
 
     public HouseCleaningRobot(string[] input)
     {
@@ -42,14 +44,66 @@ public class HouseCleaningRobot
     {
         for (int row = 0; row < _grid.Length; row++)
         {
+            _grid[row] = new Cell[input[row].Length];
             for (int column = 0; column < _grid[row].Length; column++)
             {
                 _grid[row][column] = new Cell { IsObstacle = input[row][column] == Constants.Obstacle };
                 if (input[row][column] == Constants.StartingPoint)
                 {
-                    _startingPosition = new Position(row, column);
+                    _startingPosition = new Position(row, column, StartingDirection);
                 }
             }
         }
     }
+
+    public int CleanFloor()
+    {
+        Position currentPosition = _startingPosition;
+        int numCellsMoved = 0;
+        
+        while(Visit(currentPosition))
+        {
+            var movementResult = Move(currentPosition);
+            numCellsMoved += movementResult.NumCellsMoved;
+            currentPosition = movementResult.NewPosition;
+        }
+        return numCellsMoved;
+    }
+
+    private bool Visit(Position position)
+    {
+        return _grid[position.Row][position.Column].Visit(position.Direction);
+    }
+
+    private MovementResult Move(Position currentPosition)
+    {
+        Position nextPosition = currentPosition.Direction switch
+        {
+            Direction.Left => currentPosition with { Column = currentPosition.Column - 1 },
+            Direction.Right => currentPosition with { Column = currentPosition.Column + 1 },
+            Direction.Up => currentPosition with { Row = currentPosition.Row - 1 },
+            Direction.Down => currentPosition with { Row = currentPosition.Row + 1 }
+        };
+        if (!IsValidPosition(nextPosition))
+        {
+            return new MovementResult(0, currentPosition with { Direction = Rotate(currentPosition.Direction) });
+        }
+        return new MovementResult(1, nextPosition);
+    }
+
+    private bool IsValidPosition(Position position) =>
+        !(
+        position.Row < 0 || position.Row >= _grid.Length ||
+        position.Column < 0 || position.Column >= _grid.Length ||
+        _grid[position.Row][position.Column].IsObstacle
+        );
+
+    private Direction Rotate(Direction direction) => direction switch
+    {
+        Direction.Up => Direction.Left,
+        Direction.Left => Direction.Down,
+        Direction.Down => Direction.Right,
+        Direction.Right => Direction.Up,
+        _ => throw new ArgumentException("Unknown direction found.")
+    };
 }
--- RoboCleaner/Program.cs
+++ RoboCleaner/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, Dirty House!");
+﻿using RoboCleaner;
+
+Console.WriteLine("Hello, Dirty House!");
+
+string[] input1 = ["....o.", "...o..", ".s...o", ".oo...", "......", ".....o"]; 
+string[] input2 = ["...o..", "..o...", ".....o", "o.....", ".s..o.", ".o...."];
+string[] input3 = [ "...o.", "s...o", "..o.o" ] ;
+
+Console.WriteLine(new HouseCleaningRobot(input1).CleanFloor());
+Console.WriteLine(new HouseCleaningRobot(input2).CleanFloor());
+Console.WriteLine(new HouseCleaningRobot(input3).CleanFloor());
+Console.ReadKey();
