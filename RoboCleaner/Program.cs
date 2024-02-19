ce3da5fe4fd8812802aa3c6aaf0a6bbd9faaaede
Aakash <1733759+aakashpolra@users.noreply.github.com>
2024-02-20 00:28:26 +1300

    Formatting input strings so that they are easier to visualize.

--- RoboCleaner/Program.cs
+++ RoboCleaner/Program.cs
@@ -2,9 +2,26 @@
 
 Console.WriteLine("Hello, Dirty House!");
 
-string[] input1 = ["....o.", "...o..", ".s...o", ".oo...", "......", ".....o"]; 
-string[] input2 = ["...o..", "..o...", ".....o", "o.....", ".s..o.", ".o...."];
-string[] input3 = [ "...o.", "s...o", "..o.o" ] ;
+string[] input1 = [
+    "....o.",
+    "...o..",
+    ".s...o",
+    ".oo...",
+    "......",
+    ".....o"];
+
+string[] input2 = [
+    "...o..",
+    "..o...",
+    ".....o",
+    "o.....",
+    ".s..o.",
+    ".o...."];
+
+string[] input3 = [
+    "...o.",
+    "s...o",
+    "..o.o" ] ;
 
 Console.WriteLine(new HouseCleaningRobot(input1).CleanFloor());
 Console.WriteLine(new HouseCleaningRobot(input2).CleanFloor());
