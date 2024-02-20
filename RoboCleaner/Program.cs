8ed80e9eda7841d612b808f962f316adfdbc8fbe
Aakash <1733759+aakashpolra@users.noreply.github.com>
2024-02-20 22:51:53 +1300

    Extract out InputParsingService to introduce better decoupling.

--- RoboCleaner/Program.cs
+++ RoboCleaner/Program.cs
@@ -2,6 +2,7 @@
 
 Console.WriteLine("Hello, Dirty House!");
 
+// Sample inputs
 string[] input1 = [
     "....o.",
     "...o..",
@@ -23,7 +24,15 @@ string[] input3 = [
     "s...o",
     "..o.o" ] ;
 
-Console.WriteLine(new HouseCleaningService(input1).CleanFloor());
-Console.WriteLine(new HouseCleaningService(input2).CleanFloor());
-Console.WriteLine(new HouseCleaningService(input3).CleanFloor());
+// Parse inputs and get Grid2D instances
+var inputParserService = new InputParserService();
+var grid1 = inputParserService.Parse(input1);
+var grid2 = inputParserService.Parse(input2);
+var grid3 = inputParserService.Parse(input3);
+
+// Clean floor
+Console.WriteLine(new HouseCleaningService(grid1).CleanFloor());
+Console.WriteLine(new HouseCleaningService(grid2).CleanFloor());
+Console.WriteLine(new HouseCleaningService(grid3).CleanFloor());
+
 Console.ReadKey();
--- RoboCleaner/Services/HouseCleaningService.cs
+++ RoboCleaner/Services/HouseCleaningService.cs
@@ -3,37 +3,11 @@ using RoboCleaner.Models;
 
 namespace RoboCleaner.Services;
 
-public class HouseCleaningService
+public class HouseCleaningService(Grid2D grid)
 {
-    private readonly Cell[][] _grid;
-    private const Direction StartingDirection = Direction.Right;
-    private PositionVector2D _startingPosition;
-
-    public HouseCleaningService(string[] input)
-    {
-        _grid = new Cell[input.Length][];
-        InitializeGrid(input);
-    }
-
-    private void InitializeGrid(string[] input)
-    {
-        for (int row = 0; row < _grid.Length; row++)
-        {
-            _grid[row] = new Cell[input[row].Length];
-            for (int column = 0; column < _grid[row].Length; column++)
-            {
-                _grid[row][column] = new Cell { IsObstacle = (CellType)input[row][column] == CellType.Obstacle };
-                if ((CellType)input[row][column] == CellType.StartingPoint)
-                {
-                    _startingPosition = new PositionVector2D(column, row, StartingDirection);
-                }
-            }
-        }
-    }
-
     public int CleanFloor()
     {
-        PositionVector2D currentPosition = _startingPosition;
+        PositionVector2D currentPosition = grid.StartingPosition;
         int numCellsVisited = 1;
 
         while (Visit(currentPosition))
@@ -47,7 +21,7 @@ public class HouseCleaningService
 
     private bool Visit(PositionVector2D position)
     {
-        return _grid[position.Y][position.X].Visit(position.Direction);
+        return grid.Cells[position.Y][position.X].Visit(position.Direction);
     }
 
     private MovementResult Move(PositionVector2D currentPosition)
@@ -67,15 +41,15 @@ public class HouseCleaningService
         }
 
         // Move
-        int cellsMoved = _grid[nextPosition.Y][nextPosition.X].PreviouslyVisited() ? 0 : 1;
+        int cellsMoved = grid.Cells[nextPosition.Y][nextPosition.X].PreviouslyVisited() ? 0 : 1;
         return new MovementResult(cellsMoved, nextPosition);
     }
 
     private bool IsValidPosition(PositionVector2D position) =>
         !(
-        position.Y < 0 || position.Y >= _grid.Length ||
-        position.X < 0 || position.X >= _grid[position.Y].Length ||
-        _grid[position.Y][position.X].IsObstacle
+        position.Y < 0 || position.Y >= grid.Cells.Length ||
+        position.X < 0 || position.X >= grid.Cells[position.Y].Length ||
+        grid.Cells[position.Y][position.X].IsObstacle
         );
 
     private Direction Rotate(Direction direction) => direction switch
